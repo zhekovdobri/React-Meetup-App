1f892d2df6c158173a1e88f372c870b2a217a981
Dobri Zhekov <zhekovdobri@gmail.com>
2022-01-24 12:55:49 +0100

    first commit

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+
+import MeetupList from "../components/meetups/MeetupList"; 
+
+function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(
+            'https://react-getting-started-86221-default-rtdb.firebaseio.com/meetups.json' 
+        )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            const meetups = [];
+
+            for (const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section>
+               <p>Loading...</p> 
+            </section>
+        );
+    }
+
+    return ( 
+        <section>
+            <h1>All Meetups</h1>
+            {/*<ul> 
+                {DUMMY_DATA.map((meetup)=> {
+                    return <li key={meetup.id}>{meetup.title}</li>;
+                })}
+            </ul> */}
+            <MeetupList meetups={loadedMeetups} />
+        </section>
+        );
+}
+
+export default AllMeetupsPage;
